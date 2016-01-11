48757efe8c76af4a4d4b6d75148eb1161bd8d4de
Yeming Liu <liuym13@lenovo.com>
2016-01-11 17:29:56 +0800

    first half, 1/11

--- js/09.box.jsx
+++ js/09.box.jsx
@@ -1,26 +1,45 @@
 /* global io */
-var socket = io();
-socket.emit('private message', { key: "value" });
+// var socket = io();
+// socket.emit('private message', { key: "value" });
 
 var ShoutBox = React.createClass({
+    socket: io(),
+    getInitialState: function () {
+        return {
+            shouts: []
+        };
+    },
+    componentDidMount: function () {
+        this.socket.on('connection', function (res) {
+            this.setState({shouts: res});
+            this.socket.off('connection');
+        }.bind(this));
+        this.socket.on('shout', function (shout) {
+            this.setState({shouts: this.state.shouts.concat([shout])})
+        }.bind(this));
+    },
     render: function () {
         return (
             <div className="shoutbox">
                 <h1>Hello, world! I am a shoutbox.</h1>
                 <ShoutForm />
-                <ShoutList />
+                <ShoutList shouts={this.state.shouts} />
             </div>
         );
     }
 });
 
 var ShoutList = React.createClass({
+    _renderShout: function (shout) {
+        return (
+            <Shout author={shout.author} key={shout.id}>{shout.content}</Shout>
+        )
+    },
     render: function () {
         return (
             <div className="shout-list">
                 Hello, world! I am a shout list.
-                <Shout author="Israfel">This <strong>is</strong> a shout!</Shout>
-                <Shout author="madao">This is <strong>another</strong> shout!</Shout>
+                {this.props.shouts.map(this._renderShout)}
             </div>
         );
     }
@@ -32,6 +51,11 @@ var ShoutForm = React.createClass({
             <div className="shout-form">
                 Hello, world! I am a shout form.
             </div>
+            <form className="shout-form">
+                <input type="text" placeholder="username" />
+                <input type="text" placeholder="say something..." />
+                <input type="submit" value="post" />
+            </form>
         );
     }
 });
@@ -51,7 +75,17 @@ var Shout = React.createClass({
     }
 });
 
+var shouts = [{
+    author: "Israfel",
+    content: "a shout",
+    id: "A001"
+}, {
+    author: "madao",
+    content: "another shout",
+    id: "A002"
+}];
+
 ReactDOM.render(
-    <ShoutBox />,
+    <ShoutBox shouts={shouts} />,
     document.getElementById('shoutbox')
 );
--- routes/09.box.router.js
+++ routes/09.box.router.js
@@ -9,12 +9,43 @@ module.exports = function (server) {
     
     io.on('connection', function (socket) {
         console.log('a user connected');
+        socket.emit('connection', [
+            {
+                author: "Israfel",
+                id: "I001",
+                content: " a shout from server"
+            }, {
+                author: "madao",
+                id: "M001",
+                content: " another shout from server"
+            }
+        ]);
         socket.on('disconnect', function () {
             console.log('a user disconnected');
         });
-        socket.on('private message', function (from, msg) {
-            console.log('I received a private message by ', from, ' saying ', msg);
+        socket.on('private message', function (msg) {
+            console.log('I received a private message saying ', msg);
         });
+        setTimeout(function() {
+            socket.emit('shout', {
+                    author: "bzfdu",
+                    id: "B001",
+                    content: " what's up"
+                });
+            // setTimeout(function() {
+            //     socket.emit('connection', [
+            //         {
+            //             author: "Israfel",
+            //             id: "I001",
+            //             content: " XD"
+            //         }, {
+            //             author: "madao",
+            //             id: "M001",
+            //             content: " XD"
+            //         }
+            //     ]);
+            // }, 1000);
+        }, 1000);
     });
     
     return router;
