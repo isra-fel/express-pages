4180d15ecd286f6635e7bc25b3ecb994705d08b5
Yeming Liu <liuym13@lenovo.com>
2016-02-24 16:15:48 +0800

    modularize HSVtoRGB; change s,v to 0.8

--- routes/09.box.router.js
+++ routes/09.box.router.js
@@ -7,7 +7,8 @@ module.exports = function (server) {
         log = new Log('debug', fs.createWriteStream('./log/09.box.log', { flags: 'a' })),
         stat = { online: 0 },
         CRC32 = require('crc-32'),
-        Shout;
+        Shout,
+        ColorUtil = require('../my_node_modules/color-util');
     
     initDB();
     initIO();
@@ -47,32 +48,8 @@ module.exports = function (server) {
     }
     
     function _generateColor(author, ip) {
-        function _HSVtoRGB(h, s, v) {
-            var r, g, b, i, f, p, q, t;
-            if (arguments.length === 1) {
-                s = h.s, v = h.v, h = h.h;
-            }
-            i = Math.floor(h * 6);
-            f = h * 6 - i;
-            p = v * (1 - s);
-            q = v * (1 - f * s);
-            t = v * (1 - (1 - f) * s);
-            switch (i % 6) {
-                case 0: r = v, g = t, b = p; break;
-                case 1: r = q, g = v, b = p; break;
-                case 2: r = p, g = v, b = t; break;
-                case 3: r = p, g = q, b = v; break;
-                case 4: r = t, g = p, b = v; break;
-                case 5: r = v, g = p, b = q; break;
-            }
-            return {
-                r: Math.round(r * 255),
-                g: Math.round(g * 255),
-                b: Math.round(b * 255)
-            };
-        }
         var h = (CRC32.str(author + ' ' + ip) % 180 + 180) / 360,
-            rgb = _HSVtoRGB(h, 0.6, 1);
+            rgb = ColorUtil.HSVtoRGB(h, 0.8, 0.8);
         return `rgb(${rgb.r}, ${rgb.g}, ${rgb.b})`;
     }
 
